49a800d045dfd74f035dcc640bca3aea1dd9eee8
KCBYO <keepcalmandbyo@gmail.com>
2021-07-19 23:44:35 -0500

    reverse time fmt

--- src/main.rs
+++ src/main.rs
@@ -120,7 +120,7 @@ fn download<T: Gallery>(
 
     let elapsed = chrono::Local::now().signed_duration_since(start_time);
     println!(
-        "\n{} files ({})\nElapsed time {}",
+        "\n{} files ({})\n{} elapsed",
         count,
         bytes_written.fmt_size(Conventional),
         elapsed.into_formatter(),
